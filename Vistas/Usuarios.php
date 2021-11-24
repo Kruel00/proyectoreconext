ecedf95edd5de353064bae7434a0958b1fd9eda8
Javier Castorena <kastor.kruel@gmail.com>
2021-11-23 20:51:23 -0800

    comushi

--- Vistas/Usuarios.php
+++ Vistas/Usuarios.php
@@ -30,11 +30,12 @@
         <div class="crear-solictud">
             <div class="nueva-solicitud">
                 <form action="" method="POST">
-                    
+
                     <div class="form-control">
                         <label for="slct_departamento">Seleccione Departamento</label>
                         <Select>
                             <option value="1">Seguridad Patrinmonial</option>
+                            <option value="2">Recursos Humanos</option>
                         </Select>
                     </div>
 
@@ -42,6 +43,7 @@
                         <label for="slct-tarea">Tipo de solicitud</label>
                         <select name="1" id="">
                             <option value="1">Salida de carton</option>
+                            <option value="2">Solicitud de vacaciones</option>
                         </select>
                     </div>
 
@@ -52,39 +54,111 @@
 
 
             <div class="controles-departamentos">
+                <h2>Formato Vacaciones</h2>
 
+                <div class="controles-area">
+                    <div class="form-control">
+                        <label for="">Folio: </label>
+                        <input type="text" value="A12456">
+                    </div>
+                    <div class="form-control">
+                        <label for="">Compañia: </label>
+                        <select name="" id="">
+                            <option value="1">Clover Wireless</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="controles-area">
+                    <div class="form-control">
+                        <label for="">Nombre: </label>
+                        <input type="text" name="" id="" value="Javier Castorena">
+                    </div>
+                    <div class="form-control">
+                        <label for="">Numero de empleado: </label>
+                        <input type="text" value="60663">
+                    </div>
+                </div>
+
+                <div class="controles-area">
+                    <div class="form-control">
+                        <label for="">Periodo:</label>
+                        <input type="year" name="" id="">
+                    </div>
+                    <div class="form-control">
+                        <label for="">Fecha de inicio:</label>
+                        <input type="date">
+                    </div>
+                </div>
+
+
+                <div class="controles-area">
+                    <div class="form-control">
+                        <label for="">Fecha retorno:</label>
+                        <input type="date">
+                    </div>
+                    <div class="form-control">
+                        <label for="">Semana de pago:</label>
+                        <input type="text" value="50">
+                    </div>
+                </div>
+
+                <div class="form-testo">
+                    <label for="">Observaciones:</label>
+                    <textarea name="" id="" cols="30" rows="5"></textarea>
+                </div>
+
+                <div class="form-control">
+                    <input type="submit">
+                </div>
             </div>
-
-
+            </div>
             </form>
-        </div>
+<h2>Solicitudes pendientes</h2>
 
-        <div class="tabla-datos">
+            <div class="tabla-datos">
                 <table class="table table-striped">
-                    <tr>
+                    <tr align="center">
                         <td>ID Solicitud</td>
                         <td>Descripcion</td>
                         <td>Evidencia</td>
-                        <td>ASSET</td>
+                        <td>Tipo de salida</td>
                         <td>Fecha</td>
-                        <td>Cantidad</td>
                         <td>Departamento</td>
                         <td>Actividad</td>
                         <td>Estado de solicitud</td>
-                        <td>Supervisor</td>
-                        <td>Materiales</td>
-                        <td>Mantenimiento</td>
-                        <td>Finanzas</td>
-                        <td>RH</td>
-                        <td>Direccion</td>
-                        <td>Seguridad</td>
-                        <td>SEH</td>
-                        <td></td>   
+                        <td>Estado de aprobacion</td>
                     </tr>
-                </table>
-        </div>
 
-    </div>
+                    <tr align="center">
+                        <td>1</td>
+                        <td>Envio de carton a recicladora</td>
+                        <td><a href="">Ver</a></td>
+                        <td>Salida de material</td>
+                        <td>2021-11-29</td>
+                        <td>Seguridad Patrimonial</td>
+                        <td>Salida de carton</td>
+                        <td>Pendiente de aprobacion</td>
+                        <td><a href="">Ver</a></td>
+
+                    </tr>
+                    <tr align="center">
+                        <td>2</td>
+                        <td>Aprobacion de vacaciones</td>
+                        <td><a href="">Ver</a></td>
+                        <td>Vacaciones </td>
+                        <td>2021-11-29</td>
+                        <td>Recursos Humanos</td>
+                        <td>Solicitud de vacaciones</td>
+                        <td>Pendiente de aprobacion</td>
+                        <td><a href="">Ver</a></td>
+
+                    </tr>
+
+                </table>
+            </div>
+
+        </div>
 </body>
 
 </html>
